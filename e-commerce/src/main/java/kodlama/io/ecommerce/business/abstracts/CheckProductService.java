b4c219e52ca338d826114e85054109423c979386
kadirulge <kadirulge7@gmail.com>
2023-03-19 14:07:07 +0300

    19.03.2023/refactor

--- e-commerce/src/main/java/kodlama/io/ecommerce/business/abstracts/CheckProductService.java
+++
@@ -1,9 +0,0 @@
-package kodlama.io.ecommerce.business.abstracts;
-
-import kodlama.io.ecommerce.entities.concretes.Product;
-
-
-public interface CheckProductService {
-
-    boolean isValid(Product product);
-}
