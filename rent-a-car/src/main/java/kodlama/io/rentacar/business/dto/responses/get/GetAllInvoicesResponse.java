50aae10d5c4503e15394ef645133b39cc92960b3
kadirulge <kadirulge7@gmail.com>
2023-04-24 12:46:54 +0300

    18.04.2023

--- rent-a-car/src/main/java/kodlama/io/rentacar/business/dto/responses/get/GetAllInvoicesResponse.java
+++ rent-a-car/src/main/java/kodlama/io/rentacar/business/dto/responses/get/GetAllInvoicesResponse.java
@@ -1,22 +1,25 @@
 package kodlama.io.rentacar.business.dto.responses.get;
 
-import kodlama.io.rentacar.entities.enums.State;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class GetCarResponse {
+public class GetAllInvoicesResponse {
     private int id;
-    private int modelId;
-    private int modelYear;
-    private String plate;
-    private State state;
-    private double dailyPrice;
+    private String cardHolder;
     private String modelName;
-    private String modelBrandName;
+    private String brandName;
+    private String plate;
+    private int modelYear;
+    private double dailyPrice;
+    private double totalPrice;
+    private int rentedForDays;
+    private LocalDateTime rentedAt;
 }
