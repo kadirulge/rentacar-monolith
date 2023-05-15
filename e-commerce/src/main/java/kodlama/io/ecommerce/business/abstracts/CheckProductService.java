84596f972e84fe69fd30401cb42f3060cd8522a9
kadirulge <kadirulge7@gmail.com>
2023-05-15 15:48:52 +0300

    -

--- e-commerce/src/main/java/kodlama/io/ecommerce/business/abstracts/CheckProductService.java
+++
@@ -1,9 +0,0 @@
-package kodlama.io.ecommerce.business.abstracts;
-
-import kodlama.io.ecommerce.entities.Product;
-
-
-public interface CheckProductService {
-    void validateProduct(Product product);
-    void checkIfProductExists(int id);
-}
